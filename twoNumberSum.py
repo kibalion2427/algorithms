1b4afc37fd710f30b8a367a868807b065e3e9975
roger <roger@rogers-MacBook-Pro.local>
2020-10-12 13:57:00 -0500

    py

---
+++ twoNumberSum.py
@@ -0,0 +1,18 @@
+def twoNumberSum(array, targetSum):
+  array.sort()
+  left = 0
+  right = len(array) - 1
+
+  while(left < right):
+    if(array[left] + array[right] < targetSum):
+      left += 1
+    elif(array[left] + array[right] > targetSum):
+      right += 1
+    else:
+      return [array[left], array[right]]
+  return []
+
+
+print(twoNumberSum([4, 6, 1, -3], 3))
+
+// # -4, -1, 1, 3, 5, 6, 8, 11
